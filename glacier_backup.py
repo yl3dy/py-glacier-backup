56b48877abe0156a43260c751386c6c724913098
Alexander Kiselyov <yl3gdy@archlinux.us>
2013-07-21 00:08:27 +0300

    Added README, fixed arguments

--- glacier_backup.py
+++ glacier_backup.py
@@ -1,21 +1,25 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
 from credentials import *
 from argparse import ArgumentParser
-from boto.glacier.layer2 import Layer2
+from boto.glacier.layer1 import Layer1
 from time import strftime
-
-#MY_VAULT = Layer2(aws_access_key_id=AMAZON_LOGIN,
-                  #aws_secret_access_key=AMAZON_PASSWORD).get_vault(VAULT_NAME)
 
 def upload(filename):
     """Upload a file."""
     desc = '{}, uploaded at {}'.format(filename, strftime('%Y-%m-%d'))
-    #return MY_VAULT.concurrent_create_archive_from_file(filename, desc)
     return desc
 
-def download(idnum):
+def notify_download(idnum):
     """Notify Glacier about backup download."""
+    pass
+
+def download(job_id):
+    """Actually download an archive from Glacier."""
+    pass
+
+def remove(idnum):
+    """Remove an archive from Glacier."""
     pass
 
 def main():
@@ -23,14 +27,24 @@ def main():
     parser = ArgumentParser(description='Amazon Glacier console backup tool')
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-u', '--upload', action='store_true')
-    group.add_argument('-d', '--download', action='store_true')
-    parser.add_argument('filename', type=str,
-                        help='name of file to backup/restore')
+    group.add_argument('-n', '--notify', action='store_true',
+                       help='notify Glacier about backup retrieval')
+    group.add_argument('-d', '--download', action='store_true',
+                       help='actually download archive')
+    group.add_argument('-r', '--remove', action='store_true')
+    parser.add_argument('entity_id', type=str,
+                        help='file to backup/archive id/job id')
     args = parser.parse_args()
-    if not args.download:
-        print('{}: {}'.format(args.filename, upload(args.filename)))
-    else:
-        download(args.filename)
+    if args.notify:
+        print('Download of archive {}: job {}'.format(args.entity_id,
+                                                     download(args.entity_id)))
+    elif args.download:
+        print(download(args.entity_id))
+    elif args.remove:
+        remove(args.entity_id)
+        print('Removed archive {}'.format(args.entity_id))
+    else:           # Upload is the default action
+        print('{}: {}'.format(args.entity_id, upload(args.entity_id)))
 
 if __name__ == '__main__':
     main()
